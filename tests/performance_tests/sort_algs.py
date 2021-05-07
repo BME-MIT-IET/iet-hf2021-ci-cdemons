de83ff69e88d66c8d36a0989d247ce750ee0736d
Egoago <61714141+Egoago@users.noreply.github.com>
2021-05-07 21:54:50 +0200

    some tests

--- tests/performance_tests/sort_algs.py
+++ tests/performance_tests/sort_algs.py
@@ -7,6 +7,8 @@ import numpy as np
 from contextlib import contextmanager
 import threading
 import _thread
+import warnings
+warnings.filterwarnings("error", '.*overflow.*')
 
 class TimeoutException(Exception):
     def __init__(self, msg=''):
@@ -21,14 +23,16 @@ def _get_limit(limit):
 class SortFail(Exception):
     error_count = 0
     labels = []
-    markers = ['v','x', '+', 's', 'D', 'o', '^']
+    markers = ['v', 'x', 's', 'D', 'o', '^']
+
     def __init__(self, label, handles, color='k'):
         if label not in SortFail.labels:
             marker = SortFail.markers[len(SortFail.labels)]
             SortFail.labels.append(label)
-            handles.append(Line([0],[0], marker=marker, label=label, color=color))
+            handles.append(Line([0], [0], marker=marker,
+                           label=label, color=color))
         self.marker = SortFail.markers[SortFail.labels.index(label)]
-        self.color=color
+        self.color = color
 
 @contextmanager
 def time_limit(seconds):
@@ -45,22 +49,22 @@ def time_limit(seconds):
 if __name__ == "__main__":
     mpl.rcParams['toolbar'] = 'None'
 
-    lengths = [int(10 ** (x/3))  for x in range(10)]
-    algs = [sort.merge_sort,
-            sort.quick_sort,
-            sort.bubble_sort,
-         #   sort.radix_sort,
-          #  sort.selection_sort,
-           # sort.gnome_sort,
-            sort.bogo_sort, #for the brave hearted
-            #sort.bucket_sort,
-            #sort.top_sort,
-            sort.max_heap_sort
+    lengths = [int(10 ** (x/2)) for x in range(16)]
+    algs = [sort.quick_sort,
+            sort.radix_sort,
+            sort.counting_sort,
+            sort.selection_sort,
+            sort.bucket_sort,
+            # for the brave hearted
+            sort.gnome_sort,
+            sort.bogo_sort
             ]
     labels = [alg.__name__ for alg in algs]
-    colors = [cm.get_cmap('gist_rainbow')(i) for i in np.linspace(0.0, 1.0, len(labels))]
-    handles = [Line([0],[0], color=color, lw=4, label=label) for label, color in zip(labels, colors)]
-    fig, axs = plt.subplots(1, 5, sharey=True, figsize=(25,5))
+    colors = [cm.get_cmap('gist_rainbow')(i)
+              for i in np.linspace(0.0, 1.0, len(labels))]
+    handles = [Line([0], [0], color=color, lw=4, label=label)
+               for label, color in zip(labels, colors)]
+    fig, axs = plt.subplots(1, 5, sharey=True, figsize=(25, 5))
     for plot in range(5):
         limit = plot*2
         for alg, color in zip(algs, colors):
@@ -69,34 +73,46 @@ if __name__ == "__main__":
             measurement_count = 3
             measurements = np.empty(measurement_count)
             for idx, length in enumerate(lengths):
-                arr = np.random.randint(pow(10,limit), pow(10,limit+1), length)
+                arr = np.random.randint(
+                    pow(10, limit), pow(10, limit+1), length)
                 try:
                     for measurement in range(measurement_count):
                         starttime = timer()
                         try:
-                            with time_limit(10):
+                            with time_limit(20):
                                 sorted = alg(arr)
                         except RecursionError:
                             raise SortFail('Recursion limit', handles)
                         except TimeoutException:
                             raise SortFail('Time out', handles)
+                        except AttributeError:
+                            raise SortFail('Array Structure not supported', handles)
+                        except RuntimeWarning as r:
+                            raise SortFail('Overflow', handles)
                         measurements[measurement] = timer() - starttime
                 except SortFail as e:
                     if len(durations) > 0:
                         axs[plot].plot(lengths[idx-1],
-                        durations[-1],
-                        marker=e.marker,
-                        color=e.color,
-                        zorder=2)
+                                       durations[-1],
+                                       marker=e.marker,
+                                       color=e.color,
+                                       zorder=10)
+                    else:
+                        axs[plot].plot(1,
+                                       0,
+                                       marker=e.marker,
+                                       color=e.color,
+                                       zorder=10)
                     break
                 durations.append(measurements.mean())
             axs[plot].plot(lengths[:len(durations)], durations, color=color)
         axs[plot].set_xscale('log')
         axs[plot].set_yscale('log')
         axs[plot].grid()
-        axs[plot].title.set_text(f'{"Value range: " if limit == 0 else ""}{_get_limit(limit)}-{_get_limit(limit+1)}')
+        axs[plot].title.set_text(
+            f'{"Value range: " if limit == 0 else ""}{_get_limit(limit)}-{_get_limit(limit+1)}')
     fig.legend(handles=handles, loc='upper left')
-    fig.suptitle("Sorting Algortihms", fontsize = 20)
+    fig.suptitle("Sorting Algortihms", fontsize=20)
     fig.supxlabel("Array length")
     fig.supylabel("Seconds")
     fig.savefig('tests/performance_tests/sorts.png')
